4a42570e3534234e43e68888ab00875e42e9a592
Ganesh <ganesh.asapu@icloud.com>
2022-03-31 10:24:56 -0400

    Added basic inv

--- src/TextAdventure/Main.java
+++ src/TextAdventure/Main.java
@@ -1,12 +1,18 @@
 package TextAdventure;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Main {
+	
+	static int INVSIZE = 10; //size of inventory
+	String currentRoom;
+	
+	ArrayList<String> inventory = new ArrayList<String>();
+	
 	HashMap<String, Room> roomList = new HashMap<String,Room>();
 	HashMap<String, Item> itemList = new HashMap<String,Item>();
 
 	public static void main(String[] args) {
 		
 	}
-
 }
